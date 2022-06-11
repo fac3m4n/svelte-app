70e3714230eea3756468cdaf8228323bb69bff27
Kevo <akerimberdi@gmail.com>
2022-06-11 21:29:19 +0300

    added rating and submit form

--- src/lib/FeedbackForm.svelte
+++ src/lib/FeedbackForm.svelte
@@ -1,9 +1,97 @@
 <script>
+  import { createEventDispatcher } from 'svelte';
+
+  import { v4 as uuidv4 } from 'uuid';
+
+  import Button from './Button.svelte';
+  import Card from './Card.svelte';
+  import RatingSelect from './RatingSelect.svelte';
+
+  const dispatch = createEventDispatcher();
+
+  let comment = '';
+  let btnDisabled = true;
+  let rating = 10;
+
+  let min = 10;
+  let message;
+
+  const handleInput = () => {
+    if (comment.trim().length <= min) {
+      message = `Please enter at least ${min} characters`;
+      btnDisabled = true;
+    } else {
+      message = null;
+      btnDisabled = false;
+    }
+  };
+
+  const handleSelect = (e) => (rating = e.detail);
+
+  const handleSubmit = () => {
+    if (comment.trim().length > min) {
+      const newFeedback = {
+        id: uuidv4(),
+        comment,
+        rating: +rating,
+      };
+
+      dispatch('add-feedback', newFeedback);
+    }
+  };
 </script>
 
-<form>
-  <div class="input-group">
-    <input type="text" placeholder="Why you keep coming back?" />
-    <button>Save</button>
-  </div>
-</form>
+<Card>
+  <header>
+    <h2>How would you rate your service with us?</h2>
+  </header>
+
+  <form on:submit|preventDefault={handleSubmit}>
+    <RatingSelect on:rating-select={handleSelect} />
+    <div class="input-group">
+      <input
+        type="text"
+        bind:value={comment}
+        placeholder="Why you keep coming back?"
+        on:input={handleInput}
+      />
+      <Button disabled={btnDisabled} type="submit">Save</Button>
+    </div>
+    {#if message}
+      <div class="message">{message}</div>
+    {/if}
+  </form>
+</Card>
+
+<style>
+  header {
+    max-width: 400px;
+    margin: auto;
+  }
+  header h2 {
+    font-size: 22px;
+    font-weight: 600;
+    text-align: center;
+  }
+  .input-group {
+    display: flex;
+    flex-direction: row;
+    border: 1px solid #ccc;
+    padding: 8px 10px;
+    border-radius: 8px;
+    margin-top: 15px;
+  }
+  input {
+    flex-grow: 2;
+    border: none;
+    font-size: 16px;
+  }
+  input:focus {
+    outline: none;
+  }
+  .message {
+    padding-top: 10px;
+    text-align: center;
+    color: rebeccapurple;
+  }
+</style>
