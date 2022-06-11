79da8f3f4592402b89eb624c640d05060fe997cc
Kevo <akerimberdi@gmail.com>
2022-06-11 18:47:16 +0300

    initial

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import FeedbackList from '../src/lib/FeedbackList.svelte';
+
+  let feedback = [
+    {
+      id: '1',
+      rating: 10,
+      comment:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed lobortis velit. Curabitur mattis lectus risus, eu pharetra turpis dapibus eget. Praesent hendrerit lacinia volutpat.',
+    },
+    {
+      id: '2',
+      rating: 9,
+      comment:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed lobortis velit. Curabitur mattis lectus risus, eu pharetra turpis dapibus eget. Praesent hendrerit lacinia volutpat.',
+    },
+    {
+      id: '3',
+      rating: 8,
+      comment:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed lobortis velit. Curabitur mattis lectus risus, eu pharetra turpis dapibus eget. Praesent hendrerit lacinia volutpat.',
+    },
+  ];
+</script>
+
+<main>
+  <FeedbackList {feedback} />
+</main>
---
+++ src/lib/FeedbackList.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { each } from 'svelte/internal';
+
+  export let feedback = [];
+</script>
+
+{#each feedback as fb (fb.id)}
+  <h3>{fb.comment}</h3>
+{/each}
